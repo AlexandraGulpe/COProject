968413baf9d2f9df5b11ab7a88719fe26bb55714
alexandragulpe <gulpe.alexandra@yahoo.com>
2022-05-11 19:52:11 +0300

    Start page

--- src/main/java/com/example/coproject/HelloController.java
+++ src/main/java/com/example/coproject/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.coproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> choiceBox;
+
+    private String[] algorithmsName = {"Bailey-Borwein-Plouffe", "Spigot", "Monte Carlo"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choiceBox.getItems().addAll(algorithmsName);
+
     }
 }
